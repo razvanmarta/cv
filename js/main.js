2f0e3c446be8ca6b3580e527ee17bf670c7f0907
unknown <marta.razvan@gmail.com>
2019-12-04 23:45:17 +0200

    fix doughnut

--- js/main.js
+++ js/main.js
@@ -7,6 +7,11 @@ function createChart(valueLocal, data, id) {
     type: "doughnut",
     data: data,
     options: {
+      elements: {
+        center: {
+          text: valueLocal
+        }
+      },
       responsive: true,
       legend: {
         display: false
@@ -20,27 +25,27 @@ function createChart(valueLocal, data, id) {
       }
     }
   });
-  Chart.pluginService.register({
-    beforeDraw: function(chart) {
-      var width = chart.chart.width,
-        height = chart.chart.height,
-        ctx = chart.chart.ctx;
-
-      ctx.restore();
-      var fontSize = (height / 114).toFixed(2);
-      ctx.font = fontSize + "em sans-serif";
-      ctx.textBaseline = "middle";
-
-      var text = valueLocal + "%",
-        textX = Math.round((width - ctx.measureText(text).width) / 2),
-        textY = height / 2;
-
-      ctx.fillText(text, textX, textY);
-      ctx.save();
-    }
-  });
-  // textCenter(valueLocal);
 }
+Chart.pluginService.register({
+  beforeDraw: function(chart) {
+    var width = chart.chart.width,
+      height = chart.chart.height,
+      ctx = chart.chart.ctx;
+
+    ctx.restore();
+    var fontSize = (height / 114).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+
+    var text = chart.config.options.elements.center.text + "%",
+      textX = Math.round((width - ctx.measureText(text).width) / 2),
+      textY = height / 2;
+
+    ctx.fillText(text, textX, textY);
+    ctx.save();
+  }
+});
+// textCenter(valueLocal);
 
 function textCenter(val) {}
 var data = {
